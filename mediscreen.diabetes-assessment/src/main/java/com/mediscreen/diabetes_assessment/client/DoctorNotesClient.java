4436880b2b0316888fdaa73eef296b88d8f495f9
afk..007fs <shamim.faiza@gmail.com>
2025-09-14 17:36:32 -0400

    contianer names update for docker and network configurations corrected

--- mediscreen.diabetes-assessment/src/main/java/com/mediscreen/diabetes_assessment/client/DoctorNotesClient.java
+++ mediscreen.diabetes-assessment/src/main/java/com/mediscreen/diabetes_assessment/client/DoctorNotesClient.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.mediscreen.diabetes_assessment.model.PractitionerNotes;
 
-@FeignClient(name = "doctor-notes-service", url = "${doctor.notes.service.url}")
+@FeignClient(name = "practitioner-notes-app", url = "${doctor.notes.service.url}" )
 
 public interface DoctorNotesClient {
 	 @GetMapping("/patient/{patId}")
--- mediscreen.diabetes-assessment/src/main/java/com/mediscreen/diabetes_assessment/client/PatientClient.java
+++ mediscreen.diabetes-assessment/src/main/java/com/mediscreen/diabetes_assessment/client/PatientClient.java
@@ -3,13 +3,14 @@ package com.mediscreen.diabetes_assessment.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.mediscreen.diabetes_assessment.model.Patient;
 
-@FeignClient(name = "patient-service", url = "${patient.service.url}")
+@FeignClient(name = "patient-demographics-app", url = "${patient.service.url}")
 
 public interface PatientClient {
-	@GetMapping("/patient/{id}")
+	@GetMapping("/patient/{id}" )
 //    Patient getPatientById(@PathVariable("id") String id);
 	Patient getPatientById(@PathVariable (name = "id") Long patientId);
 }
